37455844094ced3b1b69f85744b58997abdd42bc
Antonio Guillén García <119064532+AntonioGuillen123@users.noreply.github.com>
2025-02-02 20:54:21 +0100

    feat: Create journey seeder

---
+++ database/seeders/JourneySeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Journey;
+use Illuminate\Database\Seeder;
+
+class JourneySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $journeys = [
+            ['departure_id' => 1, 'arrival_id' => 2],
+            ['departure_id' => 1, 'arrival_id' => 3],
+            ['departure_id' => 2, 'arrival_id' => 4],
+            ['departure_id' => 2, 'arrival_id' => 5],
+            ['departure_id' => 3, 'arrival_id' => 6],
+            ['departure_id' => 3, 'arrival_id' => 7],
+            ['departure_id' => 4, 'arrival_id' => 8],
+            ['departure_id' => 5, 'arrival_id' => 9],
+            ['departure_id' => 6, 'arrival_id' => 10],
+            ['departure_id' => 7, 'arrival_id' => 1],
+        ];
+
+        foreach ($journeys as $journey) {
+            Journey::create($journey);
+        }
+    }
+}
