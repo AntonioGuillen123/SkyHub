6606f100aebade952fa960f74e057dbb82ad967b
Antonio Guillén García <119064532+AntonioGuillen123@users.noreply.github.com>
2025-02-02 21:54:14 +0100

    fix: Fix default role id to user

--- database/migrations/2025_02_02_201132_add_role_user_id_field_to_users_table.php
+++ database/migrations/2025_02_02_201132_add_role_user_id_field_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->foreignId('role_user_id')->default(1)->constrained('role_user')->onDelete('cascade');
+            $table->foreignId('role_user_id')->default(2)->constrained('role_user')->onDelete('cascade');
         });
     }
 
