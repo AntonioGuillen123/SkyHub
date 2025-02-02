4756b1eae5e3f8e9f855489ad2771ecdc6f8f774
Antonio Guillén García <119064532+AntonioGuillen123@users.noreply.github.com>
2025-02-02 18:08:56 +0100

    feat: Create journey model

---
+++ app/Models/Journey.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Journey extends Model
+{
+    protected $table = 'journey';
+
+    protected $fillable = [
+        'departure_id',
+        'arrival_id'
+    ];
+
+    public function destinationDeparture(){
+        return $this->belongsTo(Destination::class, 'departure_id');
+    }
+
+    public function destinationArrival(){
+        return $this->belongsTo(Destination::class, 'arrival_id');
+    }
+}
