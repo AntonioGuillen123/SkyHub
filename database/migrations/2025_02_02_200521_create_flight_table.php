01dbc86d8c5eb3be44c614e721771184b9abfd93
Antonio Guillén García <119064532+AntonioGuillen123@users.noreply.github.com>
2025-02-02 20:25:01 +0100

    feat: Create flight migration

---
+++ database/migrations/2025_02_02_200521_create_flight_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('flight', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('airplane_id')->constrained('airplane')->onDelete('cascade');
+            $table->foreignId('journey_id')->constrained('journey')->onDelete('cascade');
+            $table->timestamp('flight_date');
+            $table->boolean('state')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('flight');
+    }
+};
