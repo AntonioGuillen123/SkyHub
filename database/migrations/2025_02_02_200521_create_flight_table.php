546f18ceba8df747fad573936260c562e04a02be
Antonio Guillén García <119064532+AntonioGuillen123@users.noreply.github.com>
2025-02-02 21:01:37 +0100

    fix: Change default field on flight date

--- database/migrations/2025_02_02_200521_create_flight_table.php
+++ database/migrations/2025_02_02_200521_create_flight_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -15,7 +16,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('airplane_id')->constrained('airplane')->onDelete('cascade');
             $table->foreignId('journey_id')->constrained('journey')->onDelete('cascade');
-            $table->timestamp('flight_date');
+            $table->timestamp('flight_date')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->boolean('state')->default(1);
             $table->timestamps();
         });
