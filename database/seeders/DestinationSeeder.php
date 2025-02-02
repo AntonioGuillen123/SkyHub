6065b2aa4beb05c4894559aae0c32ab503a3350d
Antonio Guillén García <119064532+AntonioGuillen123@users.noreply.github.com>
2025-02-02 20:48:34 +0100

    feat: Create destination seeder

---
+++ database/seeders/DestinationSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Destination;
+use Illuminate\Database\Seeder;
+
+class DestinationSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $destinations = [
+            ['name' => 'New York'],
+            ['name' => 'London'],
+            ['name' => 'Paris'],
+            ['name' => 'Tokyo'],
+            ['name' => 'Dubai'],
+            ['name' => 'Sydney'],
+            ['name' => 'Toronto'],
+            ['name' => 'Madrid'],
+            ['name' => 'Berlin'],
+            ['name' => 'Rome'],
+        ];
+
+        foreach ($destinations as $destination) {
+            Destination::create($destination);
+        }
+    }
+}
