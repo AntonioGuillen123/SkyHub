dc8e96043213a6f26db66e5c74fbfe74092aa9c3
Antonio Guillén García <119064532+AntonioGuillen123@users.noreply.github.com>
2025-02-03 13:51:13 +0100

    feat: Create API airplane routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Http\Controllers\Api\AirplaneController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/airplane', [AirplaneController::class, 'index'])->name('apiIndexAirplane');
+Route::get('/airplane/{id}', [AirplaneController::class, 'show'])->name('apiShowAirplane');
+Route::post('/airplane', [AirplaneController::class, 'store'])->name('apiStoreAirplane');
+Route::put('/airplane/{id}', [AirplaneController::class, 'update'])->name('apiUpdateAirplane');
+Route::delete('/airplane/{id}', [AirplaneController::class, 'destroy'])->name('apiDestroyAirplane');
