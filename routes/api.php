d0eee4e0387f19e130a2ffb4acb8fee14d0e4f74
Antonio Guillén García <119064532+AntonioGuillen123@users.noreply.github.com>
2025-02-03 16:52:31 +0100

    feat: Create API flight routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AirplaneController;
+use App\Http\Controllers\Api\FlightController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,3 +14,9 @@ Route::get('/airplane/{id}', [AirplaneController::class, 'show'])->name('apiShow
 Route::post('/airplane', [AirplaneController::class, 'store'])->name('apiStoreAirplane');
 Route::put('/airplane/{id}', [AirplaneController::class, 'update'])->name('apiUpdateAirplane');
 Route::delete('/airplane/{id}', [AirplaneController::class, 'destroy'])->name('apiDestroyAirplane');
+
+Route::get('/flight', [FlightController::class, 'index'])->name('apiIndexFlight');
+Route::get('/flight/{id}', [FlightController::class, 'show'])->name('apiShowFlight');
+Route::post('/flight', [FlightController::class, 'store'])->name('apiStoreFlight');
+Route::put('/flight/{id}', [FlightController::class, 'update'])->name('apiUpdateFlight');
+Route::delete('/flight/{id}', [FlightController::class, 'destroy'])->name('apiDestroyFlight');
