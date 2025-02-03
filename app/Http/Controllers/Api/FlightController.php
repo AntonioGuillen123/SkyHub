8b90d411bdb6c4684fa4512d1a19c1bd1ae1451e
Antonio Guillén García <119064532+AntonioGuillen123@users.noreply.github.com>
2025-02-03 16:53:12 +0100

    feat: Create functions to response on API flight controller

---
+++ app/Http/Controllers/Api/FlightController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Flight;
+use Illuminate\Http\Request;
+
+class FlightController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Flight $flight)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Flight $flight)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Flight $flight)
+    {
+        //
+    }
+
+    private function responseWithSuccess($data, $status = 200)
+    {
+        return response()->json($data, $status);
+    }
+
+    private function responseWithError($message, $status)
+    {
+        return response()->json([
+            'message' => $message . ' :('
+        ], $status);
+    }
+}
