e89077b190de2beb4104618934fa816a923c54f1
Antonio Guillén García <119064532+AntonioGuillen123@users.noreply.github.com>
2025-02-03 21:20:40 +0100

    feat: Add transaction to flight model

--- app/Models/Flight.php
+++ app/Models/Flight.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Relations\Pivot;
 use App\Models\User;
+use Illuminate\Support\Facades\DB;
 
 class Flight extends Pivot
 {
@@ -21,11 +22,13 @@ class Flight extends Pivot
 
     public $incrementing = true;
 
-    public function airplane(){
+    public function airplane()
+    {
         return $this->belongsTo(Airplane::class);
     }
 
-    public function journey(){
+    public function journey()
+    {
         return $this->belongsTo(Journey::class);
     }
 
@@ -34,14 +37,17 @@ class Flight extends Pivot
         return $this->belongsToMany(User::class, 'booking')->withTimestamps();
     }
 
-    protected static function booted(){
-        static::updating(function (Flight $flight){
-            $state = $flight->state;
-            $remaining_places = $flight->remaining_places;
-            
-            if ($state && $remaining_places == self::MIN_PLACES_TO_DEACTIVATE_FLIGHT){
-                $flight->state = false;
-            }
+    protected static function booted()
+    {
+        static::updating(function (Flight $flight) {
+            DB::transaction(function () use ($flight) { // Funciona como una función lambda, hay que usar el use para pasar una variable del ámbito externo a el ámbito interno de la función anónima :))
+                $state = $flight->state;
+                $remaining_places = $flight->remaining_places;
+
+                if ($state && $remaining_places == self::MIN_PLACES_TO_DEACTIVATE_FLIGHT) {
+                    $flight->state = false;
+                }
+            });
         });
     }
 }
