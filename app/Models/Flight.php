04e93a673858f6928fd7c0554bf2fb032bc38761
Antonio Guillén García <119064532+AntonioGuillen123@users.noreply.github.com>
2025-02-03 21:14:12 +0100

    feat: Create logic for change status when remaining places equals 0

--- app/Models/Flight.php
+++ app/Models/Flight.php
@@ -7,6 +7,8 @@ use App\Models\User;
 
 class Flight extends Pivot
 {
+    private const MIN_PLACES_TO_DEACTIVATE_FLIGHT = 0;
+
     protected $table = 'flight';
 
     protected $fillable = [
@@ -30,5 +32,16 @@ class Flight extends Pivot
     public function users()
     {
         return $this->belongsToMany(User::class, 'booking')->withTimestamps();
+    }
+
+    protected static function booted(){
+        static::updating(function (Flight $flight){
+            $state = $flight->state;
+            $remaining_places = $flight->remaining_places;
+            
+            if ($state && $remaining_places == self::MIN_PLACES_TO_DEACTIVATE_FLIGHT){
+                $flight->state = false;
+            }
+        });
     }
 }
