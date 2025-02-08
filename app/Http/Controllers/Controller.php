1ca6e92073d2311b9a9011ccbe72c8ef70c409c8
Antonio Guillén García <119064532+AntonioGuillen123@users.noreply.github.com>
2025-02-08 16:53:32 +0100

    build: Config swagger

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,18 @@
 
 namespace App\Http\Controllers;
 
+/**
+ * @OA\Info(
+ *       title="Sky Hub API", 
+ *       version="1.0",
+ *       description="List Of API URLS"
+ * )
+ *
+ * @OA\Server(
+ *       url=L5_SWAGGER_CONST_HOST,
+ *       description="Main Server"
+ * )
+ */
 abstract class Controller
 {
     //
