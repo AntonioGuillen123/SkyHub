474b4915b630f3fcd0b701cb898068a4dd2aee3c
Antonio Guillén García <119064532+AntonioGuillen123@users.noreply.github.com>
2025-02-14 19:20:41 +0100

    feat: Add scope to cancel booking

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,6 +26,7 @@ class AppServiceProvider extends ServiceProvider
             'manage-airplanes'=> 'This scope allows manage airplanes',
             'manage-flights'=> 'This scope allows manage flights',
             'make-booking'=> 'This scope allows make a booking',
+            'cancel-booking'=> 'This scope allows cancel a booking',
             'list-flights'=> 'This scope allows list flights',
             'list-bookings' => 'This scope allows list bookings',
             'list-all-bookings' => 'This scope allows list all users bookings'
