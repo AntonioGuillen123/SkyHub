f0f414743d0791f9bf69000e4314c2fcfe20d523
Antonio Guillén García <119064532+AntonioGuillen123@users.noreply.github.com>
2025-02-14 15:25:53 +0100

    feat: Add function to validate register data

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -8,5 +8,11 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    
+    private function validateData(Request $request){
+        return $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email|max:255',
+            'password' => 'required|string|min:8|confirmed'
+        ]);
+    }
 }
