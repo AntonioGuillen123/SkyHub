6460115b20d63de457cf7c85c12faa94d4e7e2ef
Antonio Guillén García <119064532+AntonioGuillen123@users.noreply.github.com>
2025-02-17 20:40:01 +0100

    feat: Add checkRole middleware to app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\CheckUserRole;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -16,7 +17,8 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
             'scopes' => CheckScopes::class,
-            'scope' => CheckForAnyScope::class
+            'scope' => CheckForAnyScope::class,
+            'checkRole' => CheckUserRole::class
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
