20a90d6853390e4675bc7dd4cfa5d25c42d0b0c5
Antonio Guillén García <119064532+AntonioGuillen123@users.noreply.github.com>
2025-02-24 20:13:02 +0100

    test: Add test for middleware Check User Role is success

--- tests/Feature/Middleware/CheckUserRoleTest.php
+++ tests/Feature/Middleware/CheckUserRoleTest.php
@@ -2,19 +2,48 @@
 
 namespace Tests\Feature\Middleware;
 
+use App\Http\Middleware\CheckUserRole;
+use App\Models\User;
+use Database\Seeders\DatabaseSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Route;
+use Laravel\Passport\Passport;
 use Tests\TestCase;
 
 class CheckUserRoleTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    private function createTestRoute(string $role, int $status)
+    {
+        Route::middleware('checkRole:' . $role)
+            ->get('/test', function () use ($status) {
+                return response('Ok', $status);
+            });
+    }
+
+    private function authenticate()
+    {
+        $user = User::find(1);
+
+        Passport::actingAs(
+            $user
+        );
+    }
+
+
+    public function test_CheckIfMiddlewareIsSuccess()
+    {
+        $this->seed(DatabaseSeeder::class);
+
+        $this->createTestRoute('admin', 200);
+
+        $this->authenticate();
+
+        $response = $this->get('/test');
+
+        $response
+            ->assertStatus(200);
     }
 }
