47d4026bb44d093afc5182c8645aec756a51aad6
Antonio Guillén García <119064532+AntonioGuillen123@users.noreply.github.com>
2025-03-05 11:43:14 +0100

    feat: Add price field to flight seeders

--- database/seeders/FlightSeeder.php
+++ database/seeders/FlightSeeder.php
@@ -13,16 +13,16 @@ class FlightSeeder extends Seeder
     public function run(): void
     {
         $flights = [
-            ['airplane_id' => 1, 'journey_id' => 1, 'state' => 1],
-            ['airplane_id' => 2, 'journey_id' => 2, 'state' => 1],
-            ['airplane_id' => 3, 'journey_id' => 3, 'state' => 0],
-            ['airplane_id' => 1, 'journey_id' => 4, 'state' => 1],
-            ['airplane_id' => 2, 'journey_id' => 5, 'state' => 1],
-            ['airplane_id' => 3, 'journey_id' => 6, 'state' => 0],
-            ['airplane_id' => 1, 'journey_id' => 7, 'state' => 1],
-            ['airplane_id' => 2, 'journey_id' => 8, 'state' => 1],
-            ['airplane_id' => 3, 'journey_id' => 9, 'state' => 0],
-            ['airplane_id' => 1, 'journey_id' => 10, 'state' => 1],
+            ['airplane_id' => 1, 'journey_id' => 1, 'state' => 1, 'price' => 100],
+            ['airplane_id' => 2, 'journey_id' => 2, 'state' => 1, 'price' => 200],
+            ['airplane_id' => 3, 'journey_id' => 3, 'state' => 0, 'price' => 150],
+            ['airplane_id' => 1, 'journey_id' => 4, 'state' => 1, 'price' => 120],
+            ['airplane_id' => 2, 'journey_id' => 5, 'state' => 1, 'price' => 220],
+            ['airplane_id' => 3, 'journey_id' => 6, 'state' => 0, 'price' => 170],
+            ['airplane_id' => 1, 'journey_id' => 7, 'state' => 1, 'price' => 130],
+            ['airplane_id' => 2, 'journey_id' => 8, 'state' => 1, 'price' => 180],
+            ['airplane_id' => 3, 'journey_id' => 9, 'state' => 0, 'price' => 160],
+            ['airplane_id' => 1, 'journey_id' => 10, 'state' => 1, 'price' => 140],
         ];
 
         foreach ($flights as $flight) {
