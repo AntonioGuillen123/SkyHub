0d23efe0f5df11644ffb2a412002dd104a628c4d
Antonio Guillén García <119064532+AntonioGuillen123@users.noreply.github.com>
2025-03-12 22:59:49 +0100

    feat: Add home page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+<x-app-layout>
+    <div class="py-12 h-full bg-[#383E48]">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="overflow-hidden shadow-sm">
+                <div class="flex justify-center text-white text-5xl font-bold">Welcome to SkyHub!</div>
+                <div class="flex gap-8 mt-6">
+                    <div class="w-[36rem] text-white"><b>Welcome aboard!</b> At SkyHub, we make every journey seamless, comfortable, and unforgettable. Whether you're flying for business or adventure, our world-class service, affordable fares, and extensive destinations ensure you reach your dream destinations with ease.
+
+                        Book your next flight today and experience travel like never before!</div>
+                    <div class="max-w-[36rem]"><img class="w-full rounded-lg" src="images/home.jpg" alt="Flight Image"></div>
+                </div>
+                {{-- <div class="p-6 text-gray-900">
+                    ¡Bienvenido a la nueva vista en Laravel Breeze!
+                </div> --}}
+            </div>
+        </div>
+    </div>
+</x-app-layout>
