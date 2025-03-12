2b8df59008afc9b4004e2f5082ec2a85d3c8302e
Antonio Guillén García <119064532+AntonioGuillen123@users.noreply.github.com>
2025-03-12 22:12:04 +0100

    style: Add footer :)

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,24 @@
+<footer class="bg-[#36AEC2]">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 p-4 md:py-4">
+        <div class="sm:flex sm:items-center sm:justify-between">
+            <div class="flex gap-3">
+                <a href="{{ route('home') }}"
+                    class="self-center text-2xl font-bold whitespace-nowrap text-[#FFDE59]">{{ config('app.name', 'Laravel') }}
+                    |</a>
+                <div class="flex gap-4 w-20">
+                    <a href="https://github.com/AntonioGuillen123">
+                        <img src="https://skillicons.dev/icons?i=github" />
+                    </a>
+                    <a href="www.linkedin.com/in/antonio-guillen-garcia">
+                        <img src="https://skillicons.dev/icons?i=linkedin" />
+                    </a>
+                </div>
+            </div>
+            <ul class="flex flex-wrap items-center text-sm font-medium mt-4 md:mt-0 text-[#FFDE59]">
+                <li>
+                    <a href="/" class="hover:underline me-4 md:me-6">Home</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</footer>
