168a7f52d726bb330dfc5cc62453e139d3f5f8d6
Antonio Guillén García <119064532+AntonioGuillen123@users.noreply.github.com>
2025-03-13 17:47:36 +0100

    refactor: Remove shadown on home blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <div class="py-12 h-full">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="overflow-hidden shadow-sm">
+            <div class="overflow-hidden">
                 <div class="flex justify-center text-white text-4xl md:text-5xl font-bold">Welcome to SkyHub!</div>
                 <div class="flex gap-8 mt-6 flex-col-reverse px-4 md:px-0 md:flex-row">
                     <div class="max-w-[36rem] text-2xl text-white"><b>Welcome aboard!</b> At SkyHub, we make every journey seamless,
