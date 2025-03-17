a519a3b0dcc30978d00af4fa24c8dc7b4dee7a85
Antonio Guillén García <119064532+AntonioGuillen123@users.noreply.github.com>
2025-03-17 19:52:54 +0100

    feat: Add link to API Docs on navbar

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -18,6 +18,9 @@
                     <x-nav-link :href="route('indexFlight')" :active="request()->routeIs('indexFlight')">
                         {{ __('Flights') }}
                     </x-nav-link>
+                    <x-nav-link :href="url('api/documentation')">
+                        {{ __('API Docs') }}
+                    </x-nav-link>
                 </div>
             </div>
 
@@ -45,6 +48,9 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('indexFlight')" :active="request()->routeIs('indexFlight')">
                 {{ __('Flights') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="url('api/documentation')">
+                {{ __('API Docs') }}
             </x-responsive-nav-link>
         </div>
 
