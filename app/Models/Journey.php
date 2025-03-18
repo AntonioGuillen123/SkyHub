36838c490f3f0508c61cd3a0508d84979835b0c3
Antonio Guillén García <119064532+AntonioGuillen123@users.noreply.github.com>
2025-03-18 22:32:11 +0100

    feat: Add Flight_id flied to airplane and journey model

--- app/Models/Journey.php
+++ app/Models/Journey.php
@@ -28,7 +28,7 @@ class Journey extends Model
     {
         return $this->belongsToMany(Airplane::class, 'flight')
             ->using(Flight::class)
-            ->withPivot('flight_date', 'arrival_date', 'state', 'remaining_places')
+            ->withPivot('id', 'flight_date', 'arrival_date', 'state', 'remaining_places')
             ->as('flight')
             ->withTimestamps();
     }
