30bcc1acd8ae6b93885780ec25d54dab2cba2d42
Antonio Guillén García <119064532+AntonioGuillen123@users.noreply.github.com>
2025-03-18 21:15:39 +0100

    style: Add style to filters

--- resources/views/components/filters.blade.php
+++ resources/views/components/filters.blade.php
@@ -1,20 +1,20 @@
-<div class="bg-white">
-    <div>
-        <select id="filtersActive" name="filtersActive">
+<div class="flex justify-evenly items-center font-bold">
+    <div class="rounded-lg">
+        <select id="filtersActive" name="filtersActive" class="rounded-lg w-36">
             <option value="all" {{ !request()->has('state') ? 'selected' : '' }}>All</option>
             <option value="1" {{ request()->query('state') === '1' ? 'selected' : '' }}>Active</option>
             <option value="0" {{ request()->query('state') === '0' ? 'selected' : '' }}>Inactive</option>
         </select>
     </div>
     <div>
-        <select id="filtersEmpty" name="filtersEmpty">
+        <select id="filtersEmpty" name="filtersEmpty" class="rounded-lg w-36">
             <option value="all" {{ !request()->has('empty') ? 'selected' : '' }}>All</option>
             <option value="1" {{ request()->query('empty') === '1' ? 'selected' : '' }}>Empty</option>
             <option value="0" {{ request()->query('empty') === '0' ? 'selected' : '' }}>Not Empty</option>
         </select>
     </div>
     <div>
-        <select id="filtersPassed" name="filtersPassed">
+        <select id="filtersPassed" name="filtersPassed" class="rounded-lg  w-36">
             <option value="all" {{ !request()->has('passed') ? 'selected' : '' }}>All</option>
             <option value="1" {{ request()->query('passed') === '1' ? 'selected' : '' }}>Passed</option>
             <option value="0" {{ request()->query('passed') === '0' ? 'selected' : '' }}>Not Passed</option>
