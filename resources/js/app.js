46e9d988eb29a245510d245e22a7591e06084a24
Antonio Guillén García <119064532+AntonioGuillen123@users.noreply.github.com>
2025-03-18 21:09:06 +0100

    feat: Add js function to do work filters :))

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,27 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+const addChangeListener = (container, filter) => {
+    container.addEventListener('change', (element) => {
+        const urlValue = element.target.value
+        const isAll = urlValue === 'all'
+
+        let newUrl = new URL(window.location.href)
+
+        isAll
+            ? newUrl.searchParams.delete(filter)
+            : newUrl.searchParams.set(filter, urlValue)
+
+        window.location.href = newUrl
+    })
+}
+
+const activeFilters = document.querySelector('#filtersActive')
+const emptyFilters = document.querySelector('#filtersEmpty')
+const passedFilters = document.querySelector('#filtersPassed')
+
+
+addChangeListener(activeFilters, 'state')
+addChangeListener(emptyFilters, 'empty')
+addChangeListener(passedFilters, 'passed')
