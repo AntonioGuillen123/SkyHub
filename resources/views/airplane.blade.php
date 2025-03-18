de50e5a5bc23f30e261b613578af3d7b97280b60
Antonio Guillén García <119064532+AntonioGuillen123@users.noreply.github.com>
2025-03-18 21:57:48 +0100

    feat: Create airplane blade

--- resources/views/airplane.blade.php
+++ resources/views/airplane.blade.php
@@ -1,3 +1,9 @@
-<div>
-    <!-- Life is available only in the present moment. - Thich Nhat Hanh -->
-</div>
+<x-app-layout>
+    <div class="py-12 h-full">
+        <div class="flex flex-col gap-8 max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="overflow-hidden flex flex-wrap justify-center gap-8">
+               
+            </div>
+        </div>
+    </div>
+</x-app-layout>
