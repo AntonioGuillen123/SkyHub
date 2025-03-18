16f345b4de86a8494ee4bae4c566ff528439d75a
Antonio Guillén García <119064532+AntonioGuillen123@users.noreply.github.com>
2025-03-19 00:42:36 +0100

    style: Add flights to airplane blade

--- resources/views/airplane.blade.php
+++ resources/views/airplane.blade.php
@@ -8,19 +8,51 @@
                             @php
                                 $airplaneId = $flights[0]->airplane_id;
                             @endphp
-                            <div class="bg-white flex items-center justify-between p-2 rounded-xl">
-                                {{-- <div class="flex justify-between w-full pl-4"> --}}
-                                <div class="flex justify-center font-bold">{{ $airplaneId }}</div>
-                                <div class="flex justify-center w-32">{{ $airplaneName }}</div>
-                                <div class="flex justify-center">{{ $maximumPlaces }}</div>
-                                <div class="cursor-pointer">
-                                    <svg class="fill-current h-8 w-8" xmlns="http://www.w3.org/2000/svg"
-                                        viewBox="0 0 20 20">
-                                        <path fill-rule="evenodd"
-                                            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                            clip-rule="evenodd">
-                                        </path>
-                                    </svg>
+
+                            <div x-data="{ open: false }" class="bg-white rounded-xl">
+                                <div class="flex items-center justify-between p-2 cursor-pointer" @click="open = !open">
+                                    <div class="flex justify-center font-bold">Airplane #{{ $airplaneId }}</div>
+                                    <div class="flex justify-center font-bold w-32">{{ $airplaneName }}</div>
+                                    <div class="flex justify-center font-bold">{{ $maximumPlaces }}</div>
+                                    <div class="cursor-pointer">
+                                        <svg class="fill-current h-8 w-8 transition-transform duration-200"
+                                            :class="open ? 'rotate-180' : 'rotate-0'" xmlns="http://www.w3.org/2000/svg"
+                                            viewBox="0 0 20 20">
+                                            <path fill-rule="evenodd"
+                                                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                                clip-rule="evenodd">
+                                            </path>
+                                        </svg>
+                                    </div>
+                                </div>
+
+                                <div x-show="open" x-collapse class="bg-gray-100 p-2 rounded-b-xl">
+                                    <div class="p-2 text-gray-800 font-semibold">Flights:</div>
+                                    <ul class="space-y-2">
+                                        @foreach ($flights as $flight)
+                                            @php
+                                                $flightId = $flight->id;
+                                                $departure = $flight->journey->destinationDeparture->name;
+                                                $arrival = $flight->journey->destinationArrival->name;
+                                                $departureDate = date(
+                                                    'Y-m-d H:i',
+                                                    strtotime($flight->flight_date),
+                                                );
+                                                $arrivalDate = date(
+                                                    'Y-m-d H:i',
+                                                    strtotime($flight->arrival_date),
+                                                );
+                                            @endphp
+                                            <li class="p-2 bg-white shadow-sm rounded-md flex justify-between">
+                                                <span>✈️Flight #{{ $flightId }}</span>
+                                                <span>🌍{{ $departure }} → {{ $arrival }}</span>
+                                                <span class="text-sm text-gray-500">
+                                                    🕓 {{ $departureDate }} →
+                                                    {{ $arrivalDate }}
+                                                </span>
+                                            </li>
+                                        @endforeach
+                                    </ul>
                                 </div>
                             </div>
                         @endforeach
