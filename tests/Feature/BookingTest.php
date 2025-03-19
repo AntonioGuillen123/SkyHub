402d3d1dd33be54fc02051243385acdff1405d87
Antonio Guillén García <119064532+AntonioGuillen123@users.noreply.github.com>
2025-03-19 22:48:32 +0100

    test: Test to make booking on web

--- tests/Feature/BookingTest.php
+++ tests/Feature/BookingTest.php
@@ -2,19 +2,40 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
+use Database\Seeders\DatabaseSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class BookingTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    private function authenticate(int $idUser)
+    {
+        $user = User::find($idUser);
+
+        $this->actingAs($user);
+    }
+
+    public function test_CheckIfPostAnEntryOfBookingInWeb()
+    {
+        $this->seed(DatabaseSeeder::class);
+
+        $this->authenticate(2);
+
+        $data = [
+            'flight_id' => 1
+        ];
+
+        $response = $this->post(route('makeBooking'), $data);
+
+        $resultMessage = 'The flight has been booked successfully';
+        $resultMessageType = 'success';
+
+        $response
+        ->assertRedirect(route('indexFlight'))
+        ->assertSessionHas('message', $resultMessage)
+        ->assertSessionHas('messageType', $resultMessageType);
     }
 }
