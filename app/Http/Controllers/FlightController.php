4b0c7f3cd23505afd16ca0dd611e661fb42d5d16
Antonio Guillén García <119064532+AntonioGuillen123@users.noreply.github.com>
2025-03-19 22:00:51 +0100

    refactor: Refactor Web Flight Controller

--- app/Http/Controllers/FlightController.php
+++ app/Http/Controllers/FlightController.php
@@ -14,6 +14,7 @@ class FlightController extends Controller
     public function index(Request $request)
     {
         $queryParams = $this->getQueryParams($request);
+        
         $userId = $this->getUserId();
 
         $flights = $this->getFlights($userId, $queryParams);
