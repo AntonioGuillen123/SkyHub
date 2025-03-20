f874c3e0c9ca04d8615485aaf522a296b4bf45f3
Antonio Guillén García <119064532+AntonioGuillen123@users.noreply.github.com>
2025-03-20 01:46:01 +0100

    test: Test to check if role user has users relationships

--- tests/Feature/Model/RoleUserTest.php
+++ tests/Feature/Model/RoleUserTest.php
@@ -2,19 +2,22 @@
 
 namespace Tests\Feature\Model;
 
+use App\Models\RoleUser;
+use App\Models\User;
+use Database\Seeders\DatabaseSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class RoleUserTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    public function test_CheckIfRoleUserHasUsersRelationship(){
+        $this->seed(DatabaseSeeder::class);
+
+        $roleUser = RoleUser::find(1);
+
+        $this->assertInstanceOf(User::class, $roleUser->users[0]);
     }
 }
