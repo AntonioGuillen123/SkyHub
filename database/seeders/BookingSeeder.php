cdab6f28639a482e8b39986dae81ce3be75a90ab
Antonio Guillén García <119064532+AntonioGuillen123@users.noreply.github.com>
2025-03-20 21:37:27 +0100

    feat: Change booking seeders

--- database/seeders/BookingSeeder.php
+++ database/seeders/BookingSeeder.php
@@ -13,48 +13,33 @@ class BookingSeeder extends Seeder
     public function run(): void
     {
         $bookings = [
-            [
-                'user_id' => 1, 
-                'flight_id' => 1,
-            ],
-            [
-                'user_id' => 2, 
-                'flight_id' => 2,
-            ],
-            [
-                'user_id' => 3, 
-                'flight_id' => 3,
-            ],
-            [
-                'user_id' => 4, 
-                'flight_id' => 4,
-            ],
-            [
-                'user_id' => 5, 
-                'flight_id' => 5,
-            ],
-            [
-                'user_id' => 6, 
-                'flight_id' => 6,
-            ],
-            [
-                'user_id' => 7, 
-                'flight_id' => 7,
-            ],
-            [
-                'user_id' => 8, 
-                'flight_id' => 8,
-            ],
-            [
-                'user_id' => 9, 
-                'flight_id' => 9,
-            ],
-            [
-                'user_id' => 10, 
-                'flight_id' => 10,
-            ],
+            ['user_id' => 2, 'flight_id' => 1],
+            ['user_id' => 2, 'flight_id' => 3],
+            ['user_id' => 2, 'flight_id' => 5],
+            ['user_id' => 2, 'flight_id' => 7],
+            ['user_id' => 2, 'flight_id' => 9],
+            ['user_id' => 4, 'flight_id' => 2],
+            ['user_id' => 4, 'flight_id' => 4],
+            ['user_id' => 4, 'flight_id' => 6],
+            ['user_id' => 4, 'flight_id' => 8],
+            ['user_id' => 4, 'flight_id' => 10],
+            ['user_id' => 6, 'flight_id' => 1],
+            ['user_id' => 6, 'flight_id' => 2],
+            ['user_id' => 6, 'flight_id' => 3],
+            ['user_id' => 6, 'flight_id' => 4],
+            ['user_id' => 6, 'flight_id' => 5],
+            ['user_id' => 6, 'flight_id' => 6],
+            ['user_id' => 8, 'flight_id' => 7],
+            ['user_id' => 8, 'flight_id' => 8],
+            ['user_id' => 8, 'flight_id' => 9],
+            ['user_id' => 8, 'flight_id' => 10],
+            ['user_id' => 10, 'flight_id' => 1],
+            ['user_id' => 10, 'flight_id' => 3],
+            ['user_id' => 10, 'flight_id' => 5],
+            ['user_id' => 10, 'flight_id' => 7],
+            ['user_id' => 10, 'flight_id' => 9]
         ];
-        
+
         foreach ($bookings as $booking) {
             $booking['created_at'] = now();
             $booking['updated_at'] = now();
