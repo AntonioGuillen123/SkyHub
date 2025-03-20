f8570943cf0bf619d6bbea2aeaab7037452c7520
Antonio Guillén García <119064532+AntonioGuillen123@users.noreply.github.com>
2025-03-20 21:16:32 +0100

    feat: Change journey seeders

--- database/seeders/JourneySeeder.php
+++ database/seeders/JourneySeeder.php
@@ -15,14 +15,24 @@ class JourneySeeder extends Seeder
         $journeys = [
             ['departure_id' => 1, 'arrival_id' => 2],
             ['departure_id' => 1, 'arrival_id' => 3],
-            ['departure_id' => 2, 'arrival_id' => 4],
-            ['departure_id' => 2, 'arrival_id' => 5],
-            ['departure_id' => 3, 'arrival_id' => 6],
-            ['departure_id' => 3, 'arrival_id' => 7],
-            ['departure_id' => 4, 'arrival_id' => 8],
+            ['departure_id' => 1, 'arrival_id' => 4],
+            ['departure_id' => 1, 'arrival_id' => 5],
+            ['departure_id' => 2, 'arrival_id' => 6],
+            ['departure_id' => 2, 'arrival_id' => 7],
+            ['departure_id' => 2, 'arrival_id' => 8],
+            ['departure_id' => 3, 'arrival_id' => 9],
+            ['departure_id' => 3, 'arrival_id' => 10],
+            ['departure_id' => 4, 'arrival_id' => 6],
+            ['departure_id' => 4, 'arrival_id' => 7],
+            ['departure_id' => 5, 'arrival_id' => 8],
             ['departure_id' => 5, 'arrival_id' => 9],
             ['departure_id' => 6, 'arrival_id' => 10],
-            ['departure_id' => 7, 'arrival_id' => 1],
+            ['departure_id' => 6, 'arrival_id' => 1],
+            ['departure_id' => 7, 'arrival_id' => 3],
+            ['departure_id' => 7, 'arrival_id' => 5],
+            ['departure_id' => 8, 'arrival_id' => 2],
+            ['departure_id' => 8, 'arrival_id' => 4],
+            ['departure_id' => 9, 'arrival_id' => 10]
         ];
 
         foreach ($journeys as $journey) {
