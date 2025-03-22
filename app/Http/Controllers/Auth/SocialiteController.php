426a896bd1fe6681e30b98312e733ba9a5447573
Antonio Guillén García <119064532+AntonioGuillen123@users.noreply.github.com>
2025-03-22 04:04:44 +0100

    feat: Add logic to authenticate with socialite

--- app/Http/Controllers/Auth/SocialiteController.php
+++ app/Http/Controllers/Auth/SocialiteController.php
@@ -4,35 +4,108 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Laravel\Socialite\Contracts\Provider;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Contracts\User as SocialiteUser;
 use Laravel\Socialite\Facades\Socialite;
 
 class SocialiteController extends Controller
 {
-    public function redirect(string $service){
-        $socialiteProvider = $this->getSocialiteProviderFromService($service);
+    public function redirect(string $provider)
+    {
+        $socialiteProvider = $this->getSocialiteProvider($provider);
 
         $providerRedirect = $this->getProviderRedirect($socialiteProvider);
 
         return $providerRedirect;
     }
 
-    public function callback(string $service){
-        $socialiteProvider = $this->getSocialiteProviderFromService($service);
+    public function callback(string $provider)
+    {
+        $socialiteProvider = $this->getSocialiteProvider($provider);
 
-        $providerUser = $this->getProviderUser($socialiteProvider);
+        $userProvider = $this->getProviderUser($socialiteProvider);
 
-        return response()->json($providerUser);
+        $userData = $this->getUserDataFromUserProvider($userProvider);
+
+        $user = $this->getUserFromEmail($userData['email']);
+
+        if (!$user) {
+            $user = $this->createUser($userData);
+        }
+
+        $this->createOrUpdateAuthProviderFromUser($user, $userData, $provider);
+
+        $this->authUser($user);
+
+        return $this->responseWithSuccess();
     }
 
-    private function getSocialiteProviderFromService(string $service){
-        return Socialite::driver($service);
+    private function getSocialiteProvider(string $provider)
+    {
+        return Socialite::driver($provider);
     }
 
-    private function getProviderRedirect(Provider $provider){
+    private function getProviderRedirect(Provider $provider)
+    {
         return $provider->redirect();
     }
 
-    private function getProviderUser(Provider $provider){
+    private function getProviderUser(Provider $provider)
+    {
         return $provider->user();
+    }
+
+    private function getUserDataFromUserProvider(SocialiteUser $userProvider)
+    {
+        $providerId = $userProvider->getId();
+        $providerEmail = $userProvider->getEmail();
+        $providerNickname = $userProvider->getNickname();
+
+        return [
+            'provider_id' => $providerId,
+            'email' => $providerEmail,
+            'nickname' => $providerNickname ?? $this->generateNickname($providerEmail),
+        ];
+    }
+
+    private function generateNickname(string $providerEmail)
+    {
+        return explode('@', $providerEmail)[0];
+    }
+
+    private function getUserFromEmail(string $email)
+    {
+        return User::where('email', $email)->first();
+    }
+
+    private function createUser(mixed $data)
+    {
+        return User::create([
+            'email' => $data['email'],
+            'name' => $data['nickname'],
+            'email_verified_at' => now()
+        ]);
+    }
+
+    private function createOrUpdateAuthProviderFromUser(User $user, mixed $data, string $provider)
+    {
+        $user->authProviders()->updateOrCreate([
+            'provider_name' => $provider
+        ], [
+            'provider_id' => $data['provider_id'],
+            'nickname' => $data['nickname'],
+            'login_at' => now()
+        ]);
+    }
+
+    private function authUser(User $user)
+    {
+        Auth::login($user);
+    }
+
+    private function responseWithSuccess()
+    {
+        return redirect(route('home'));
     }
 }
