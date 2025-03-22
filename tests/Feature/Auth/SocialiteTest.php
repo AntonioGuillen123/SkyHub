a937eac233c0296d0e0bfe2eed99978688c00d09
Antonio Guillén García <119064532+AntonioGuillen123@users.noreply.github.com>
2025-03-22 17:02:38 +0100

    test: Test to check if socialite callback

--- tests/Feature/Auth/SocialiteTest.php
+++ tests/Feature/Auth/SocialiteTest.php
@@ -2,9 +2,11 @@
 
 namespace Tests\Feature\Auth;
 
+use Database\Seeders\DatabaseSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Laravel\Socialite\Facades\Socialite;
+use Laravel\Socialite\Contracts\User as SocialiteUser;
 use Illuminate\Support\Str;
 use Laravel\Socialite\Contracts\Provider;
 use Mockery;
@@ -28,6 +30,32 @@ class SocialiteTest extends TestCase
             ->andReturn($mockProvider);
 
         $response = $this->get("/auth/{$provider}/redirect");
+
+        $response->assertRedirect();
+    }
+
+    public function test_example(): void
+    {
+        $this->seed(DatabaseSeeder::class);
+
+        $provider = 'github';
+
+        $abstractUser = Mockery::mock(SocialiteUser::class);
+        $abstractUser->shouldReceive('getId')->andReturn('123456789');
+        $abstractUser->shouldReceive('getEmail')->andReturn('test@example.com');
+        $abstractUser->shouldReceive('getNickname')->andReturn('testuser');
+
+        $mockProvider = Mockery::mock(Provider::class);
+        $mockProvider->shouldReceive('user')
+            ->once()
+            ->andReturn($abstractUser);
+
+        Socialite::shouldReceive('driver')
+            ->once()
+            ->with($provider)
+            ->andReturn($mockProvider);
+
+        $response = $this->get("/auth/{$provider}/callback");
 
         $response->assertRedirect();
     }
