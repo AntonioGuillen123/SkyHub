510d82f58283322acbc8d8fb13fd6c73f2fc2699
Antonio Guillén García <119064532+AntonioGuillen123@users.noreply.github.com>
2025-03-22 04:57:06 +0100

    test: Test to check if authenticated user can create a password

---
+++ tests/Feature/Auth/PasswordCreateTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Tests\Feature\Auth;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Hash;
+use Tests\TestCase;
+
+class PasswordCreateTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_password_can_be_created(): void
+    {
+        $user = User::factory()->create();
+
+        $response = $this
+            ->actingAs($user)
+            ->from('/profile')
+            ->put('/create-password', [
+                'password' => 'new-password',
+                'password_confirmation' => 'new-password',
+            ]);
+
+        $response
+            ->assertSessionHasNoErrors()
+            ->assertRedirect('/profile');
+
+        $this->assertTrue(Hash::check('new-password', $user->refresh()->password));
+    }
+
+    public function test_correct_password_must_be_provided_to_create_password(): void
+    {
+        $user = User::factory()->create();
+
+        $response = $this
+            ->actingAs($user)
+            ->from('/profile')
+            ->put('/create-password', [
+                'password' => 'new-password',
+                'password_confirmation' => 'new-wrong-password',
+            ]);
+
+        $response
+            ->assertSessionHasErrorsIn('createPassword', 'password')
+            ->assertRedirect('/profile');
+    }
+}
