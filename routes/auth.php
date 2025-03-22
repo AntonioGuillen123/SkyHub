ee118e01ad0c5d6089e209645f81fdf561513b66
Antonio Guillén García <119064532+AntonioGuillen123@users.noreply.github.com>
2025-03-22 04:28:39 +0100

    feat: Add route to create password fot oauth users

--- routes/auth.php
+++ routes/auth.php
@@ -54,6 +54,8 @@ Route::middleware('auth')->group(function () {
 
     Route::put('password', [PasswordController::class, 'update'])->name('password.update');
 
+    Route::put('create-password', [PasswordController::class, 'store'])->name('password.create');
+
     Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
         ->name('logout');
 });
