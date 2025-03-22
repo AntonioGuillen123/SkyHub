c0ee009ba0a4c07febecd17d2bbe510e6a71a968
Antonio Guillén García <119064532+AntonioGuillen123@users.noreply.github.com>
2025-03-22 01:02:25 +0100

    feat: Add socialite redirect function

--- app/Http/Controllers/Auth/SocialiteController.php
+++ app/Http/Controllers/Auth/SocialiteController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Laravel\Socialite\Facades\Socialite;
 
 class SocialiteController extends Controller
 {
-    //
+    public function redirect(string $provider){
+        return Socialite::driver($provider)->redirect();
+    }
 }
