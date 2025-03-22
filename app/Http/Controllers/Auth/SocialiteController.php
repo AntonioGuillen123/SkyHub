5c8dafa6df2cad184c756c9dafc28a1c5be31c57
Antonio Guillén García <119064532+AntonioGuillen123@users.noreply.github.com>
2025-03-22 01:11:19 +0100

    refactor: Refactor redirect function

--- app/Http/Controllers/Auth/SocialiteController.php
+++ app/Http/Controllers/Auth/SocialiteController.php
@@ -2,11 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use Laravel\Socialite\Contracts\Provider;
 use Laravel\Socialite\Facades\Socialite;
 
 class SocialiteController extends Controller
 {
-    public function redirect(string $provider){
-        return Socialite::driver($provider)->redirect();
+    public function redirect(string $service){
+        $socialiteProvider = $this->getSocialiteProviderFromService($service);
+
+        $providerRedirect = $this->getProviderRedirect($socialiteProvider);
+
+        return $providerRedirect;
+    }
+
+    private function getSocialiteProviderFromService(string $service){
+        return Socialite::driver($service);
+    }
+
+    private function getProviderRedirect(Provider $provider){
+        return $provider->redirect();
+    }
+
+    private function getProviderUser(Provider $provider){
+        return $provider->user();
     }
 }
